4567021871af44393b93f770128761e171c736d0
Ja-R <jacqueline_rwa@msn.com>
2018-03-05 14:31:47 +0100

    bk

--- MVC/model.php
+++ MVC/model.php
@@ -5,7 +5,7 @@ function dbConnect()
 {
   try
   {
-    $db = new PDO('mysql:host=127.0.0.1;dbname=test;charset=utf8', 'root', 'user', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+    $db = new PDO('mysql:host=127.0.0.1;dbname=blogsuperlab;charset=utf8', 'root', 'user', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
     return $db;
   }
   catch (Exception $e)
@@ -20,11 +20,11 @@ function login()
 {
 
   $db = dbConnect();
+  if (!isset($_POST['user']) && (!isset($_POST['pwd']))) {
 
-  $req = $db->prepare('SELECT id, login, password,
-  FROM utilisateur');
-  $req->execute(array());
-  $post= $req->fetch();
-
-return $post;
+  }
+  $req = $db->prepare('SELECT login, pass FROM utilisateurs WHERE login = :pseudo, pass = :pass');
+  $req->execute(array(
+      'login' => $login));
+  $resultat = $req->fetch();
 }
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # filrougeasteam
+
+
+### [Consignes](https://github.com/becodeorg/Swartz-promo-3/blob/master/Projects/filrougeasteam.md) :
+Réalisation de la partie blog du one page créé lors du [Guerilla Style](https://github.com/becodeorg/Swartz-promo-3/blob/master/Projects/filrouge.md) en mode team (collaborateurs: Jonathan D., Corentin N. et Jacqueline R.)
